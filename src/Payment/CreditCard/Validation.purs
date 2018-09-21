c50e56cea74d3a11e72c7c6e0e4d62b9aca27d89
Alexej Bondarenko <alexej.bondarenko@ersocon.net>
2018-09-21 12:58:05 +0200

    Add implementation and tests

--- src/Payment/CreditCard/Validation.purs
+++ src/Payment/CreditCard/Validation.purs
@@ -1,30 +1,56 @@
 module Payment.CreditCard.Validation where
   
-import Prelude (map, (==))
+import Prelude
 
-import Data.String.Common
+import Data.String.Common (split)
 import Data.String.Pattern (Pattern(..))
-import Data.Int (fromString)
+import Data.Int (fromString, even, toNumber)
 import Data.Maybe (isJust, fromMaybe)
-import Data.Array (length, filter)
+import Data.Array (dropEnd, last, length, filter, reverse, toUnfoldable)
+import Data.Foldable (sum)
+import Math ((%))
+import Data.FunctorWithIndex (mapWithIndex)
 
 
 
 isValidInput :: String -> Boolean
-isValidInput creditCardNumber =
+isValidInput creditCardNumber = isValidArray (convertToNumbers creditCardNumber)
+
+
+
+isValidArray :: Array Int -> Boolean
+isValidArray [] = false
+isValidArray [x] = false
+isValidArray numbers =
   let
-    numbers = convertToNumbers creditCardNumber
-  in 
-    if (length numbers == 0) then
-      false
-    else
-      isValidArray numbers
+    reversed = reverse (dropEnd 1 numbers)
+    verificationNumber = fromMaybe 0 (last numbers)
+  in
+    luhnCheck reversed verificationNumber
 
 
 
-isValidArray :: Array Int -> Bool
-isValidArray arr =
-  false
+doubleEachSecond :: Array Int -> Array Int
+doubleEachSecond = mapWithIndex (\idx item -> if (even idx) then item * 2 else item)
+
+
+
+luhnCorrection :: Array Int -> Array Int
+luhnCorrection = map (\number -> if (number < 10) then number else (number - 9) )
+
+
+
+luhnNumbers :: Array Int -> Array Int
+luhnNumbers input = luhnCorrection (doubleEachSecond input)
+
+
+
+luhnCheck :: Array Int -> Int -> Boolean
+luhnCheck input verificationNumber =
+  (luhnSum input) % (toNumber 10) == (toNumber (10 - verificationNumber))
+  where
+    luhnSum :: Array Int -> Number
+    luhnSum numbersArr = toNumber (sum (luhnNumbers numbersArr))
 
 
 
@@ -32,9 +58,9 @@ convertToNumbers :: String -> Array Int
 convertToNumbers creditCardNumber =
    let 
      numbers = map (fromString) (split (Pattern "") creditCardNumber)
-     unparsed = filter isJust numbers
+     unparsed = filter (not isJust) numbers
    in
      if (length unparsed) == 0 then
-       map (fromMaybe 0) numbers
+       toUnfoldable (map (fromMaybe 0) numbers)
      else
-       []
+       toUnfoldable []
--- test/Main.purs
+++ test/Main.purs
@@ -1,34 +1,74 @@
 module Test.Main where
   
-import Prelude (Unit, discard)
-
 import Effect
 
+import Payment.CreditCard.Validation as PCV
+import Prelude (Unit, discard)
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (run)
 
-import Payment.CreditCard.Validation as PCV
+import Data.Array (reverse)
 
 
 
 main :: Effect Unit
 main = run [consoleReporter] do
-  describe "Payment.CreditCard.Validation (isValid)" do
+  describe "Payment.CreditCard.Validation" do
     describe "isValidInput" do
-        it "Should validate valid credit card numbers as valid" do
-          (PCV.isValidInput "4716292509375978") `shouldEqual` true
+      it "Should validate valid credit card number 4916864428336303" do
+        (PCV.isValidInput "4916864428336303") `shouldEqual` true
+
+      it "Should validate valid credit card number 4485186432004102" do
+        (PCV.isValidInput "4485186432004102") `shouldEqual` true
+
+      it "Should validate valid credit card number 5498671212144998" do
+        (PCV.isValidInput "5498671212144998") `shouldEqual` true
+
+      it "Should validate valid credit card number 4556737586899855" do
+        (PCV.isValidInput "4556737586899855") `shouldEqual` true
         
-        it "Should validate invalid credit card numbers as invalid" do
-          (PCV.isValidInput "4716292509375979") `shouldEqual` false
+      it "Should validate invalid credit card numbers as invalid" do
+        (PCV.isValidInput "4716292509375979") `shouldEqual` false
 
-        it "Should reject non digit inputs" do
-          (PCV.isValidInput "471629vv09375979") `shouldEqual` false
+      it "Should reject non digit inputs" do
+        (PCV.isValidInput "471629vv09375979") `shouldEqual` false
 
-        it "Should reject 0 input" do
-          (PCV.isValidInput "0") `shouldEqual` false  
+      it "Should reject 0 input" do
+        (PCV.isValidInput "0") `shouldEqual` false  
 
+    describe "doubleEachSecond" do
+      it "should double each second number in an array" do
+        (PCV.doubleEachSecond [1,1,1,1,1]) `shouldEqual` [2,1,2,1,2]
+        (PCV.doubleEachSecond [1,1,1,1,1,1]) `shouldEqual` [2,1,2,1,2,1]
+
+    describe "luhnCorrection" do
+      it "should correct numbers higher than 9" do
+        (PCV.luhnCorrection [1,12,5,14,1,9,0]) `shouldEqual` [1,3,5,5,1,9,0]
+
+    describe "luhnCheck" do
+      it "should yield correct result for [4,5,1,3,2,9,2,1,0,9,3,5,5,9,7,7] 2" do  
+        (PCV.luhnCheck [4,5,1,3,2,9,2,1,0,9,3,5,5,9,7,7] 2) `shouldEqual` true
+
+      it "should yield correct result for reverse [4,5,5,6,7,3,7,5,8,6,8,9,9,8,5]) 5" do  
+        (PCV.luhnCheck (reverse [4,5,5,6,7,3,7,5,8,6,8,9,9,8,5]) 5) `shouldEqual` true
+
+      it "should yield correct result for reverse [4,9,1,6,8,6,4,4,2,8,3,3,6,3,0]) 3" do  
+        (PCV.luhnCheck (reverse [4,9,1,6,8,6,4,4,2,8,3,3,6,3,0]) 3) `shouldEqual` true
+
+    describe "isValidArray" do
+      
+
+      it "should be valid for [4,5,5,6,7,3,7,5,8,6,8,9,9,8,5,5]" do
+        (PCV.isValidArray [4,5,5,6,7,3,7,5,8,6,8,9,9,8,5,5]) `shouldEqual` true
+
+      it "should be valid for [4,9,1,6,8,6,4,4,2,8,3,3,6,3,0,3]" do
+        (PCV.isValidArray [4,9,1,6,8,6,4,4,2,8,3,3,6,3,0,3]) `shouldEqual` true
+
+    describe "convertToNumbers" do
+      it "should parse digits correctly" do
+        (PCV.convertToNumbers "4716292509375978" `shouldEqual` [4,7,1,6,2,9,2,5,0,9,3,7,5,9,7,8])
 
 
 validCreditCardNumbers :: Array String
